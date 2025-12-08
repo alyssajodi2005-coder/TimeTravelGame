2c3fb3efa86e1fa96662aaa5d6ee33b80a24c096
Grish Kadel <grishkadel35@gmail.com>
2025-12-08 12:12:45 -0500

    changes to player class

--- headers/player.h
+++ headers/player.h
@@ -26,7 +26,41 @@ class Player{
 
 
     public: 
-        
+    
+    // Constructor to initialize player with a name
     Player(const string& user_name);
+    
+    // Getter methods
+    
+    // Get player's name (constant because the player's name should be the same since the start)
+    string get_name() const;
+    // Get current year
+    int get_year();
+    // Get time battery level
+    int get_time_battery();
+    // Get player's score
+    int get_score();
+    // Get player's money
+    double get_money();
+    // Get player's inventory
+    vector<string> get_inventory();
+    // Get decades visited
+    vector<int> get_decades_visited();
+
+    // Setter methods
+
+    // Set current year
+    void set_year(int new_year);
+    // Set time battery level
+    void set_time_battery(int new_time_battery);
+    // Set player's score
+    void set_score(int new_score);
+    // Set player's money
+    void set_money(double new_money);
+    // Add item to inventory
+    void add_to_inventory(string item);
+    // Add decade to decades visited
+    void add_decade_visited(int decade);
+
 };
 #endif // PLAYER_H
