f3ff1b1543d8b63e467d7fa0c8261a3ae9e1c7b0
Alyssa Johnson <alyssajosi2005@gmail.com>
2025-12-07 22:33:11 -0500

    Merged parent's changes

--- headers/player.h
+++ headers/player.h
@@ -32,32 +32,3 @@ class Player{
 };
 #endif // PLAYER_H
 =======
-#include <iostream>
-#include <vector>
-using namespace std;
-
-
-class Player{
-    // Private member variables 
-
-    // Time Battery indicates how much time travel energy the player has left;
-    int time_battery;
-    // Name of the player
-    string name;
-    // Current year the player is in
-    int year;
-    // Number of decades the player has visited
-    vector<int> decades_visited;
-    // Player's score
-    int score;
-    // Amount of money the player has
-    double money;
-    // Inventory items the player carries
-    vector<string> inventory;
-
-
-    public: 
-        
-    Player(string user_name);
-};
->>>>>>> ca33911e0be546a7a16cf5eee2117a711ec81b3d
